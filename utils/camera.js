bc4463297438c4ad866b7ce5adb49a4dab030099
turtlehat <89934210+turtle-hat@users.noreply.github.com>
2026-06-08 23:14:40 -0400

    Start 01-01_FirstScene

---
+++ utils/camera.js
@@ -0,0 +1,7 @@
+// Camera WASD movement
+camera.keysUp.push(87);
+camera.keysDown.push(83);
+camera.keysLeft.push(65);
+camera.keysRight.push(68);
+camera.keysUpward.push(69);
+camera.keysDownward.push(81);
